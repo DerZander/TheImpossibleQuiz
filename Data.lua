d367152ef96a375ff28fc6731a913abc448b559f
Timo Zander <timo.zander@outlook.com>
2026-06-13 23:43:15 +0200

    Add new quiz question about a round sports ball and update existing questions for consistency

--- Data.lua
+++ Data.lua
@@ -6,85 +6,90 @@ TeekesselchenQuizData = {
             correctExplanation = "Hammer",
             wrongExplanation = "es eine besonders starke Leistung beschreiben."
         },
-	{
-	    question = "Was ist ein Nadelbaum?",
-	    word = "Kiefer",
-	    correctExplanation = "Kiefer",
-	    wrongExplanation = "es ein Knochen im menschlichen Körper ist."
-	},
-	{
-	    question = "Was ist ein Baustellenfahrzeug?",
-	    word = "Raupe",
-	    correctExplanation = "Raupe",
-	    wrongExplanation = "es eine Vorstufe des Schmetterlings ist."
-	},
-	{
-	    question = "Wie nennt man eine längere Aneinanderreihung von bewegten Bildern?",
-	    word = "Film",
-	    correctExplanation = "Film",
-	    wrongExplanation = "es extrem dünne, zusammenhängende Schicht einer Flüssigkeit auf einer Oberfläche oder Flüssigkeit."
-	},
-	{
-	    question = "Wie nennt man den Kopfschmuck eines Königs?",
-	    word = "Krone",
-	    correctExplanation = "Krone",
-	    wrongExplanation = "es ein Zahnersatz ist."
-	},
-	{
-	    question = "Wie nennt man ein weißes Pferd?",
-	    word = "Schimmel",
-	    correctExplanation = "Schimmel",
-	    wrongExplanation = "es ein Pilzbefall ist."
-	},
-	{
-	    question = "Was ist ein Tier und gleichzeitig eine mediale Falschmeldung?",
-	    word = "Ente",
-	    correctExplanation = "Ente",
-	    wrongExplanation = "es ein französiches Fahrzeug ist."
-	},
-	{
-	    question = "Wie nennt man einen länglichen Teppich?",
-	    word = "Läufer",
-	    correctExplanation = "Läufer.",
-	    wrongExplanation = "es ein Sportler ist."
-	},
-	{
-	    question = "Wie nennt man ein Fahrzeug auf Schienen?",
-	    word = "Zug",
-	    correctExplanation = "Zug.",
-	    wrongExplanation = "es eine Art der Luftbewegung ist."
-	},
-	{
-	    question = "Wie nennt man ein Schmuckstück, das man am Finger trägt?",
-	    word = "Ring",
-	    correctExplanation = "Ring",
-	    wrongExplanation = "Ring eine Firma ist, die Sicherheitssysteme wie Überwachungskameras herstellt."
-	},
-	{
-	    question = "Wie nennt man einen sehr starken Fluss?",
-	    word = "Strom",
-	    correctExplanation = "Strom",
-	    wrongExplanation = "es die elektrische Energie aus der Steckdose ist."
-	},
-	{
-	    question = "Wie nennt man den Körperteil zwischen Kopf und Schultern?",
-	    word = "Hals",
-	    correctExplanation = "Hals",
-	    wrongExplanation = "es der oberen schmalen Teil einer Flasche ist."
-	},
-	{
-	    question = "Wie nennt man ein Werkzeug zum Öffnen einer Tür?",
-	    word = "Schlüssel",
-	    correctExplanation = "Schlüssel",
-	    wrongExplanation = "es als Zeichen in der Musik die Tonart anzeigt."
-	},
-	{
-	    question = "Wie nennt man ein Geldinstitut, bei dem man ein Konto eröffnen kann?",
-	    word = "Bank",
-	    correctExplanation = "Bank",
-	    wrongExplanation = "es eine Sitzgelegenheit bezeichnen."
-	}
-
+        {
+            question = "Was ist ein Nadelbaum?",
+            word = "Kiefer",
+            correctExplanation = "Kiefer",
+            wrongExplanation = "es ein Knochen im menschlichen Körper ist."
+        },
+        {
+            question = "Was ist ein Baustellenfahrzeug?",
+            word = "Raupe",
+            correctExplanation = "Raupe",
+            wrongExplanation = "es eine Vorstufe des Schmetterlings ist."
+        },
+        {
+            question = "Wie nennt man eine längere Aneinanderreihung von bewegten Bildern?",
+            word = "Film",
+            correctExplanation = "Film",
+            wrongExplanation = "es extrem dünne, zusammenhängende Schicht einer Flüssigkeit auf einer Oberfläche oder Flüssigkeit."
+        },
+        {
+            question = "Wie nennt man den Kopfschmuck eines Königs?",
+            word = "Krone",
+            correctExplanation = "Krone",
+            wrongExplanation = "es ein Zahnersatz ist."
+        },
+        {
+            question = "Wie nennt man ein weißes Pferd?",
+            word = "Schimmel",
+            correctExplanation = "Schimmel",
+            wrongExplanation = "es ein Pilzbefall ist."
+        },
+        {
+            question = "Was ist ein Tier und gleichzeitig eine mediale Falschmeldung?",
+            word = "Ente",
+            correctExplanation = "Ente",
+            wrongExplanation = "es ein französiches Fahrzeug ist."
+        },
+        {
+            question = "Wie nennt man einen länglichen Teppich?",
+            word = "Läufer",
+            correctExplanation = "Läufer.",
+            wrongExplanation = "es ein Sportler ist."
+        },
+        {
+            question = "Wie nennt man ein Fahrzeug auf Schienen?",
+            word = "Zug",
+            correctExplanation = "Zug.",
+            wrongExplanation = "es eine Art der Luftbewegung ist."
+        },
+        {
+            question = "Wie nennt man ein Schmuckstück, das man am Finger trägt?",
+            word = "Ring",
+            correctExplanation = "Ring",
+            wrongExplanation = "Ring eine Firma ist, die Sicherheitssysteme wie Überwachungskameras herstellt."
+        },
+        {
+            question = "Wie nennt man einen sehr starken Fluss?",
+            word = "Strom",
+            correctExplanation = "Strom",
+            wrongExplanation = "es die elektrische Energie aus der Steckdose ist."
+        },
+        {
+            question = "Wie nennt man den Körperteil zwischen Kopf und Schultern?",
+            word = "Hals",
+            correctExplanation = "Hals",
+            wrongExplanation = "es der oberen schmalen Teil einer Flasche ist."
+        },
+        {
+            question = "Wie nennt man ein Werkzeug zum Öffnen einer Tür?",
+            word = "Schlüssel",
+            correctExplanation = "Schlüssel",
+            wrongExplanation = "es als Zeichen in der Musik die Tonart anzeigt."
+        },
+        {
+            question = "Wie nennt man ein Geldinstitut, bei dem man ein Konto eröffnen kann?",
+            word = "Bank",
+            correctExplanation = "Bank",
+            wrongExplanation = "es eine Sitzgelegenheit bezeichnen."
+        },
+        {
+            question = "Wie nennt man ein rundes Spielgerät beim Sport?",
+            word = "Ball",
+            correctExplanation = "Ball",
+            wrongExplanation = "es eine Tanzveranstaltung ist."
+        }
     }
 }
 
